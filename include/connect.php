7c002067adcd16be49465e2aa14c18fc46dfa712
GiangSon <giangson0208@gmail.com>
2019-03-16 10:11:40 -0700

    update 9/3/2019

--- include/connect.php
+++ include/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$link=mysql_connect("localhost","root","") or die("Cannot connect to the localhost");
+	$link=mysql_connect("localhost","root","123456") or die("Cannot connect to the localhost");
 	mysql_select_db("trasua",$link) or die("Cannot connect to the database");
 	mysql_query("SET NAMES 'UTF8'");
 ?>
